50b43a9eecd9c1c990af96292d7f275474f447e6
iGitScor <sebastien.correaud@gmail.com>
2016-12-16 10:54:11 +0100

    Add manual call to replace content

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ module.exports = class ContentReplacer {
     this.modifiedFile = options.modifiedFile;
   }
 
+  replace() {
+    const that = this;
+    function replaceInFile(filePath, toReplace, replacement) {
+      function replacer(match) {
+        const debug = '\x1b[1m\x1b[34mReplacing in %s: %s => %s\x1b[0m';
+        console.log(debug, filePath, match, replacement);
+
+        return replacement;
+      }
+
+      const str = fs.readFileSync(filePath, 'utf8');
+      const out = str.replace(new RegExp(toReplace), replacer);
+      fs.writeFileSync(filePath, out);
+    }
+
+    if (Array.isArray(this.modifications) && this.modifications.length > 0) {
+      [].forEach.call(this.modifications, (modif) => {
+        replaceInFile(that.modifiedFile, modif.regex, modif.modification);
+      });
+    }
+  }
+
   apply(compiler) {
     if (this.modifications && this.modifiedFile) {
       const that = this;
       compiler.plugin('after-emit', (compilation, callback) => {
-        function replaceInFile(filePath, toReplace, replacement) {
-          function replacer(match) {
-            const debug = '\x1b[1m\x1b[34mReplacing in %s: %s => %s\x1b[0m';
-            console.log(debug, filePath, match, replacement);
-
-            return replacement;
-          }
-
-          const str = fs.readFileSync(filePath, 'utf8');
-          const out = str.replace(new RegExp(toReplace), replacer);
-          fs.writeFileSync(filePath, out);
-        }
-
-        if (Array.isArray(this.modifications) && this.modifications.length > 0) {
-          [].forEach.call(this.modifications, (modif) => {
-            replaceInFile(that.modifiedFile, modif.regex, modif.modification);
-          });
-        }
-
+        that.replace();
         callback();
       });
     }
