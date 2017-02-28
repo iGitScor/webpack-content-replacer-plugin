fd7d52f76edb41e01ee99dd68ba3d703e8568251
Yan, Qun <yq1119apzz@gmail.com>
2017-02-28 16:38:38 -0500

    add test case

---
+++ test/testcase-test.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const expect = require('expect.js');
+const webpack = require('webpack');
+const config = require('./../workflow_test/webpack.config.js');
+const reverse = require('./../workflow_test/webpack.config.reverse.js');
+
+describe('Webpack workflow test cases', () => {
+  afterEach(() => {
+    webpack(reverse, (err, stats) => {
+      if (err) return err;
+      if (stats.hasErrors()) return new Error(stats.toString());
+      return stats;
+    });
+  });
+
+  it('should make new build', (done) => {
+    webpack(config, (err, stats) => {
+      if (err) return done(err);
+      if (stats.hasErrors()) return done(new Error(stats.toString()));
+
+      expect(stats).to.not.be.empty();
+      return done();
+    });
+  });
+
+  it('should replace element value in html file', (done) => {
+    webpack(config, (err, stats) => {
+      if (err) return done(err);
+      if (stats.hasErrors()) return done(new Error(stats.toString()));
+
+      const testFilePath = path.join(__dirname, '../workflow_test/index.html');
+      const target = ['Replaced', 'Replaced2'];
+      const data = fs.readFileSync(testFilePath, 'utf8').toString();
+      target.forEach((value) => {
+        expect(data.indexOf(value) !== -1).to.equal(true);
+      });
+      return done();
+    });
+  });
+
+  it('should replace style value in css file', (done) => {
+    webpack(config, (err, stats) => {
+      if (err) return done(err);
+      if (stats.hasErrors()) return done(new Error(stats.toString()));
+
+      const testCssPath = path.join(__dirname, '../workflow_test/style.css');
+      const target = ['Replaced', 'Replaced2'];
+      const data = fs.readFileSync(testCssPath, 'utf8').toString();
+      target.forEach((value) => {
+        expect(data.indexOf(value) !== -1).to.equal(true);
+      });
+      return done();
+    });
+  });
+});
